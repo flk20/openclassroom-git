666b20756eeeb1c8299a89779a7674419bcd9a23
fabio.ferreira3 <ffem@hcuge.ch>
2019-01-21 08:54:43 +0100

    Ajout d'un constructure ansi que d'une méthode static sayHello pour afficher un text entré en paramètre

--- App.java
+++ App.java
@@ -1,5 +1,21 @@
 public class App{
+
+	
+	private Strinf text;
+
+	public App(String text){
+		this.text = text;
+	}
+
+
+	public void sayHello(String text){
+		System.out.println(this.text);
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Hello Word");
+		
+		App test = new App("Hello");
+		test.sayHello;
+
 	}
 }
